0b7cd386da6bdb8bb39aa0fe13c4823854f89c33
Jeremy Yan <jeremy@yansc.com>
2015-03-22 00:35:59 -0400

    updated script to exclude meanFreq

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,6 @@
 # This is course project by Shuangchun Yan
 
 ### Part One: Read In Data
-
 activity_label <- read.table(file="UCI HAR Dataset/activity_labels.txt", sep = " ")
 features <- read.table(file="UCI HAR Dataset/features.txt", sep = " ")
 
@@ -9,27 +8,25 @@ features <- read.table(file="UCI HAR Dataset/features.txt", sep = " ")
 X_test <- read.table(file="UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table(file="UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table(file="UCI HAR Dataset/test/subject_test.txt")
-
 # train
 X_train <- read.table(file="UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table(file="UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table(file="UCI HAR Dataset/train/subject_train.txt")
 # covert Y_test numbers into activity feature 
 
-# make a copy
-Y_test1 <- Y_test
-Y_test1$features <- sapply(Y_test1[,1], function(x) {
+# Going through each of the activity 
+Y_test$activity <- sapply(Y_test[,1], function(x) {
       activity_label[activity_label[,1] == x,][1,2]
 })
 
-Y_train1 <- Y_train
-Y_train1$features <- sapply(Y_train1[,1], function(x) {
+Y_train$activity <- sapply(Y_train[,1], function(x) {
       activity_label[activity_label[,1] == x,][1,2]
 })
 
 # change the column names
-names(Y_test1) <- c("activity_label", "activity")
-names(Y_train1) <- c("activity_label", "activity")
+names(Y_test) <- c("activity_label", "activity")
+names(Y_train) <- c("activity_label", "activity")
+
 # change the X_test with proper column names
 names(X_test) <- features[,2]
 names(X_train) <- features[,2]
@@ -39,43 +36,48 @@ names(subject_test) <- "subject"
 names(subject_train) <- "subject"
 
 # combining these data
-data_test <- cbind(subject_test, Y_test1, X_test)
-data_train <- cbind(subject_train, Y_train1, X_train)
-
+data_test <- cbind(subject_test, Y_test, X_test)
+data_train <- cbind(subject_train, Y_train, X_train)
+# combina train and test dataset
 data <- rbind(data_test, data_train)
 
-names_to_keep <- names(data)
+# get the names of all col
+names_to_keep <- colnames(data)
 # keep only those contain either "mean" or "std"
 names_to_keep <- names_to_keep[c(1:3, grep("mean", names_to_keep), grep("std", names_to_keep))]
+# remove meanFreq becasue there is no std associated with it, so it is not true "mean" of measurement
+names_to_keep <- names_to_keep[-(grep("meanFreq", names_to_keep))]
 names_to_keep <- sort(names_to_keep)
 
+# keep only the columns to keep
 data_tidy <- data[,names_to_keep]
 #move subject to front
 data_tidy <- subset(data_tidy, select = c("subject", names_to_keep[names_to_keep != "subject"]))
 # order by subject number
-data_tidy2 <- data_tidy[order(data$subject),]
+data_tidy <- data_tidy[order(data$subject),]
 
-# calculate the mean for each subject and activity
-subjects <- 1:30
+## calculate the mean for each subject and activity
+
+# get all possible subjects and activity for looping
+subjects <- unique(data_tidy$subject)
 activity <- activity_label[,2]
 
-# data_tidy3 <- lapply(unique(data_tidy2$subject), function(x) {
-#       data_tidy2[data_tidy2[,1] == x]
-# })
-
-tidy_data3 <- data.frame(matrix(NA, nrow = 0, ncol = (length(colnames(data_tidy2))-1)))
+# create an empty data.frame with set dimension, -1 because I will get ride of activity_label col
+data_final <- data.frame(matrix(NA, nrow = 0, ncol = (length(colnames(data_tidy))-1)))
 
 # loop through each subject
 for(i in subjects) { 
-      subject_data <- data_tidy2[data_tidy2$subject == i,]
+      subject_data <- data_tidy[data_tidy$subject == i,]
       # for each subject, loop through each activity to get means
       for (j in activity) {
             subject_activity <- subject_data[subject_data$activity == j,]
             avg_subject_activity <- colMeans(subject_activity[4:length(names_to_keep)])
-            tidy_data3 <- rbind(tidy_data3, data.frame(t(c(i, j, avg_subject_activity))))
+            data_final <- rbind(data_final, data.frame(t(c(i, j, avg_subject_activity))))
       }
 }
 
 # the other column names were given by colMeans, so add the labels to the first two col
-names(tidy_data3)[1:2] <- c("subject", "activity")
-
+names(data_final) <- c("subject", "activity", names_to_keep[4:length(names_to_keep)])
+head(data_final)
+dim(data_final)
+write.table(data_final, file = "data_final.txt", row.name=FALSE)
